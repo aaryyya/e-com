1cd640ac7965a7d7dc879fcfe0c1dafaafa2b60c
aaryyya <aryampatil2005@gmail.com>
2025-09-25 09:39:57 +0530

    DB configs and Controller and model updated

---
+++ src/main/java/com/e_com/ecom/Controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.e_com.ecom.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @RequestMapping("/")
+    public String greet(){
+        return "Heloo People";
+    }
+}
