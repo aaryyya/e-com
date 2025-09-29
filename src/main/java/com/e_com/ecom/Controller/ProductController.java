6ea1c21094d718c7571642fcae513a36f7c276f3
aaryyya <aryampatil2005@gmail.com>
2025-09-29 20:23:15 +0530

    Added @CrossOrigin for connecting to react frontend

--- src/main/java/com/e_com/ecom/Controller/ProductController.java
+++ src/main/java/com/e_com/ecom/Controller/ProductController.java
@@ -3,6 +3,7 @@ package com.e_com.ecom.Controller;
 import com.e_com.ecom.Model.Product;
 import com.e_com.ecom.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
     @Autowired
