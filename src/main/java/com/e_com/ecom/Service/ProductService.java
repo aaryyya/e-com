20063b580b54fb6b711e438496e60999516e2273
aaryyya <aryampatil2005@gmail.com>
2025-09-29 19:29:35 +0530

    Repo interface added for using JPA and its metthod add the class types and the primary key data type

--- src/main/java/com/e_com/ecom/Service/ProductService.java
+++ src/main/java/com/e_com/ecom/Service/ProductService.java
@@ -1,8 +1,18 @@
 package com.e_com.ecom.Service;
 
+import com.e_com.ecom.Model.Product;
+import com.e_com.ecom.Repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+    @Autowired
+    private ProductRepo repo;
+    public List<Product> getAllProducts() {
+        return repo.findAll();
 
+    }
 }
