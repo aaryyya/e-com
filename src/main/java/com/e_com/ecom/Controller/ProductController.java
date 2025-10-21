959e7eabe20c439ee9a57aa72a0d3010945aedb5
aaryyya <aryampatil2005@gmail.com>
2025-10-21 22:12:41 +0530

    addProduct along with image sucessfully done the adding of image but some error in adding description might need few changes in frontend in fetching in react will be changed in next commit

--- src/main/java/com/e_com/ecom/Controller/ProductController.java
+++ src/main/java/com/e_com/ecom/Controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -32,5 +33,18 @@ public class ProductController {
 //        return service.getProductById(id);//
     }
 
-//    @PostMapping()
+    @PostMapping("/product")//just product coz we r sending data in the body
+    public ResponseEntity<?>addProduct(@RequestPart Product product,
+                                       @RequestPart MultipartFile imageFile){//for just normal product without an image it is easy as we just use request body
+        //?represents that it can either return data or an exception
+        //request body accepts the whole object or the whole json as object
+        //request part accepts in the parts
+        try {
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1,HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(e.getMesssage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/e_com/ecom/Service/ProductService.java
+++ src/main/java/com/e_com/ecom/Service/ProductService.java
@@ -4,7 +4,9 @@ import com.e_com.ecom.Model.Product;
 import com.e_com.ecom.Repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -18,5 +20,12 @@ public class ProductService {
     public Product getProductById(int id){
         return repo.findById(id).orElse(null);//use if using with response entity
 //        return repo.findById(id).get();//use if doing without response entity
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImgeName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repo.save(product);
     }
 }
